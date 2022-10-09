e6bcdba31f6c2f28e322ec46e870718374dbc30e
Khachig-Gabriel <khachiggabriel@gmail.com>
2022-10-10 00:51:07 +0400

    Import products to Armenia and calculate the import taxes (If the product's price exceeds 200 000 AMD).

--- src/Import.java
+++ src/Import.java
@@ -1,37 +1,32 @@
 public class Import {
     public static void main(String[] args) {
-
+        /*
+        If you live in Armenia and want to buy and import a product to Armenia (Amazon/Ebay etc.).
+        This program helps you calculate the total cost of the product, tax included.
+         */
         double x = 1295;
         print(x);
-
     }
 
-    public static double convert (double dram)
-    {
-        return (dram*418);
-    }
+    public static double convert (double dram)  { return (dram*418); }
 
     public static double tax(double dram) {
         if (convert(dram) > 200_000) {
-            return ((convert(dram) - 200_000) * 0.15);} else {
-        return 0; }}
+            return ((convert(dram) - 200_000) * 0.15);}
+        else {
+            return 0; }}
 
     public static double allFees(double dram){
         if (convert(dram) > 200_000) {
             double inflated = ((convert(dram)-200_000) * 0.15);
-            return convert(dram) + inflated;
-        } else {
-            return convert(dram);
-        }
-    }
-
+            return convert(dram) + inflated; }
+        else { return convert(dram); } }
 
     public static void print(double dram) {
         System.out.println("USD: " + dram);
         System.out.println("Added Tax (AMD): " + tax(dram));
         System.out.println("Final price (AMD): " + allFees(dram));
-        System.out.println("\n");
-    }
+        System.out.println("\n"); }
 
 }
 
